ea8cd4eb6e0319e9597fb747a7534c53cb16204a
Rune K. Svendsen <runesvend@gmail.com>
2022-05-27 14:56:36 +0200

    make it a derivation

--- release.nix
+++ release.nix
@@ -1,4 +1,17 @@
 let
-  x = 9193746282;
+  nixpkgs =
+    let
+      src = builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/30d3d79b7d3607d56546dd2a6b49e156ba0ec634.tar.gz";
+        sha256 = "0x5j9q1vi00c6kavnjlrwl3yy1xs60c34pkygm49dld2sgws7n0a";
+      };
+    in import src {};
 in
-  x
+nixpkgs.stdenv.mkDerivation {
+  name = "test-repo-smithy";
+  src = ./data;
+  installPhase = ''
+    mkdir -p $out
+    cp data.bin $out/
+  '';
+}
