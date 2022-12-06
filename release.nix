68973544ac8b7a48e12622d289c21d35260c4d35
Rune K. Svendsen <runesvend@gmail.com>
2022-12-06 14:14:03 +0100

    Make release.nix contain a single IFD

--- release.nix
+++ release.nix
@@ -1,20 +1,27 @@
+# IFD example
+# Cf. https://nixos.wiki/wiki/Import_From_Derivation
 let
-  nixpkgs =
+  pkgs =
     let
       src = builtins.fetchTarball {
         url = "https://github.com/NixOS/nixpkgs/archive/30d3d79b7d3607d56546dd2a6b49e156ba0ec634.tar.gz";
         sha256 = "0x5j9q1vi00c6kavnjlrwl3yy1xs60c34pkygm49dld2sgws7n0a";
       };
     in import src {};
-  nixpkgsDiamond = import <nixpkgs> {};
-  lol = "pay no attention to this let-binding!!!1";
-in
-nixpkgs.stdenv.mkDerivation {
-  name = "test-repo-smithy";
-  src = ./data;
-  installPhase = ''
-    mkdir -p $out
-    cp data.txt $out/
-    echo "${nixpkgsDiamond.stdenv.system}" > $out/system
+
+  # Create a derivation which, when built, writes some Nix code to
+  # its $out path.
+  derivation-to-import = pkgs.writeText "ifd" ''
+    pkgs: pkgs.stdenv.mkDerivation {
+      name = "test-repo-smithy-ifd";
+      src = pkgs.fetchurl {
+        url = "https://github.com/runeksvendsen/test-repo-smithy/archive/79fb1e22122775a5b4718e1919768d02333df252.tar.gz";
+        sha256 = "sha256:0yvmw2nd5apps7g8lpsxd0k6d7y8kyd9zvd4prwh5bfi8zxxn3r5";
+      };
+      installPhase = "mkdir -p $out ; cp data/data.txt $out/ ; echo 'ifd test' > $out/ifd.txt";
+    }
   '';
-}
+
+  # Import this derivation.
+  imported-derivation = import derivation-to-import;
+in imported-derivation pkgs
